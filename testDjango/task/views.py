596c58ab72dc45e80bb5b9c84d1314f1cd73b0b4
ptitto <paulustito555@gmail.com>
2023-02-16 15:11:49 +0700

    Add

--- testDjango/task/views.py
+++ testDjango/task/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+import include
 # Create your views here.
+task = ["foo", "bar", "baz"]
+def index(request):
+    return render(request, "task/index.html", {
+        "task":task
+    })
